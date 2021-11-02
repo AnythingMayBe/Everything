86c285d1c74cf084cd5b6872e15c1a82b742ab4d
AnythingMayBe <anythingmaybe@protonmail.com>
2021-11-02 16:17:12 +0100

    Removed ping command

--- Everything.cpp
+++ Everything.cpp
@@ -8,16 +8,10 @@ void handle(std::string raw_event) {
 
 int main()
 {
-    dpp::cluster bot("ODc3NTEzMTQ2Njk5NDg5Mjgw.YRztzQ.3wySCx2EzbACSRCll2M9HIBqsQs");
+    dpp::cluster bot("token");
  
     bot.on_ready([&bot](const dpp::ready_t & event) {
         std::cout << "Logged in as " << bot.me.username << "!\n";
-    });
- 
-    bot.on_message_create([&bot](const dpp::message_create_t & event) {
-        if (event.msg->content == "!ping") {
-            bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
-        }
     });
 
    
