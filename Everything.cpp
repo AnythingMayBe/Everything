4ff1ce860765663e787b533d5d2ac23f51890709
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-26 17:44:59 +0100

    Inited bot

---
+++ Everything.cpp
@@ -0,0 +1,17 @@
+#include <dpp/dpp.h>
+#include <iostream>
+
+int main() {
+    // Init
+    dpp::cluster bot("ODc4NTgwNDU2NzA5MjkyMDUz.YSDP0A.5108bZ_8eR5izYq8iinTvRDqV1Q");
+
+    bot.on_ready([&bot](const auto & event) {
+        std::cout << "Logged in as " << bot.me.username << std::endl;
+    });
+
+    
+
+    // Start bot
+    bot.start(false);
+    return 0;
+}
