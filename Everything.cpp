315a26cb6a1fd0a67dc20c396ec47cc049f8faec
AnythingMayBe <anythingmaybe@protonmail.com>
2022-01-26 17:56:11 +0100

    Added Webhook Init

--- Everything.cpp
+++ Everything.cpp
@@ -5,6 +5,14 @@ int main() {
     // Init
     dpp::cluster bot("ODc4NTgwNDU2NzA5MjkyMDUz.YSDP0A.5108bZ_8eR5izYq8iinTvRDqV1Q");
 
+    // Webhook Init
+    dpp::webhook wh;
+    wh.id = 935940551105196042; // The ID of your Webhook
+    wh.token = "CBG3AWBvApyTYlJolLG9LAkHOcXN5t6zZdHrX-qOZ7RrnmoW3LZN4waMsqhHhTTH3gKH"; // The token of your webhook
+    
+
+    // Events
+
     bot.on_ready([&bot](const auto & event) {
         std::cout << "Logged in as " << bot.me.username << std::endl;
     });
